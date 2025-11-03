3595bc0a29d8c072061b2400c97f44ea0ea8c69e
Sweilam <alisweilem2004@gmail.com>
2025-11-03 16:27:05 +0200

    Initial commit

---
+++ src/main/java/com/example/librarymanager/App.java
@@ -0,0 +1,11 @@
+package com.example.librarymanager;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
