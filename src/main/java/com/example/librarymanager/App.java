f43bf21d849427f1c276fbc121e3b363288724f8
Sweilam <alisweilem2004@gmail.com>
2025-11-04 21:03:04 +0200

    Fix bug in login UI

--- src/main/java/com/example/librarymanager/App.java
+++ src/main/java/com/example/librarymanager/App.java
@@ -2,7 +2,9 @@ package com.example.librarymanager;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -10,9 +12,13 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        Parent root = fxmlLoader.load();
+
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.setScene(scene);
-        stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
     }
 }
